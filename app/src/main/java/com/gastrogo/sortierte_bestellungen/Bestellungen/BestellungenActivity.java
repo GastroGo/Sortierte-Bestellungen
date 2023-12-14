c5b25e7ed1c8c5e4d6562409b08174ce4f8a0c60
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2023-12-14 16:00:34 +0100

    init commit

---
+++ app/src/main/java/com/gastrogo/sortierte_bestellungen/Bestellungen/BestellungenActivity.java
@@ -0,0 +1,16 @@
+package com.gastrogo.sortierte_bestellungen.Bestellungen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gastrogo.sortierte_bestellungen.R;
+
+public class BestellungenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bestellungen);
+    }
+}
---
+++ app/src/main/java/com/gastrogo/sortierte_bestellungen/Tische/TischeActivity.java
@@ -0,0 +1,16 @@
+package com.gastrogo.sortierte_bestellungen.Tische;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gastrogo.sortierte_bestellungen.R;
+
+public class TischeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tische);
+    }
+}
